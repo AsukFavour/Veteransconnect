0a443683b1b5e049e6e7d9e5626dd4c4f33c223d
AsukFavour <favourasuk@gmail.com>
2023-06-10 17:19:17 +0100

    about us

--- src/component/AboutUs/AboutUs.jsx
+++ src/component/AboutUs/AboutUs.jsx
@@ -1,33 +1,29 @@
-import React from 'react';
-import './AboutUs.css';
+import React from "react";
+import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us">
       <h1 className="about-us__heading">About Us</h1>
       <p className="about-us__text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae tellus ac sapien
-        tristique faucibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
-        cubilia Curae; Aliquam pretium euismod nulla, quis euismod neque. Donec non sodales massa.
-        Nam nec nisi dolor. Morbi quis augue velit. Nam suscipit aliquet quam a bibendum. Sed
-        eleifend justo quis sagittis aliquet. In hac habitasse platea dictumst. Suspendisse
-        potenti. Vivamus eu lectus eget risus facilisis suscipit.
+        Welcome to Veteran Benevolent Support Africa, a platform created to
+        serve retired military veterans. Our mission is to provide comprehensive
+        support and resources to empower veterans in their service journey. We
+        believe in enhancing the well-being, personal growth, and professional
+        success of veterans. Through our platform, veterans have access to a
+        wide range of resources that can improve that.
       </p>
       <p className="about-us__text">
-        Aenean id mollis risus. Ut ornare, quam sit amet ornare tincidunt, mauris magna consequat
-        velit, eget ullamcorper enim nulla eu nulla. Vestibulum et congue odio. Sed euismod lacus
-        enim, a tristique nulla lacinia vel. Proin quis mollis libero. Pellentesque habitant morbi
-        tristique senectus et netus et malesuada fames ac turpis egestas. Sed vestibulum purus vel
-        turpis auctor aliquet. Nam id tortor eget mi congue mollis. Fusce ut ante justo. Mauris nec
-        nibh in enim dignissim euismod vitae ac libero.
+        At Veteran Benevolent Support Africa, we understand the unique
+        challenges faced by retired military veterans. Our user-friendly
+        platform offers a range of features and services to address diverse
+        needs. From health awareness and education to education and training,
+        entrepreneurship courses, employment, support services and much more.
       </p>
       <p className="about-us__text">
-        Nulla eu ante a lacus eleifend tincidunt. Suspendisse potenti. Donec ut ligula at erat
-        facilisis dignissim id eu tellus. Nunc ut fringilla eros. Proin ut dolor ultrices,
-        ullamcorper orci ut, commodo mi. Nullam scelerisque, magna ac tristique molestie, massa
-        risus ullamcorper ex, id luctus risus odio quis lorem. Aliquam ut consequat nisi. Morbi vel
-        orci tellus. Donec ultricies a urna vel lobortis. Donec nec nisl sit amet metus bibendum
-        blandit. Etiam lobortis dolor purus, in finibus ex bibendum vel.
+        Join us as we provide comprehensive support to retired military
+        veterans. Together, we can make a difference in the lives of veterans in
+        Africa.
       </p>
     </div>
   );
