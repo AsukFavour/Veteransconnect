a6f2bb1c2d6754d508e4f363e24e94589bd1c13b
AsukFavour <favourasuk@gmail.com>
2023-06-10 17:28:04 +0100

    about us

--- src/component/AboutPage/About.jsx
+++ src/component/AboutPage/About.jsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import './About.css';
-import Navbar from '../TopNavbar/Navbar';
-import Footer from '../Footer/Footer';
-import about from '../../assets/image.jpg'
+import React from "react";
+import "./About.css";
+import Navbar from "../TopNavbar/Navbar";
+import Footer from "../Footer/Footer";
+import about from "../../assets/about.jpg";
 
 const About = () => {
   return (
     <>
-    <Navbar/>
-    <div className="about">
-      <div className="about-header">
-        <img src={about} alt="Header Image" />
+      <Navbar />
+      <div className="about">
+        <div className="about-header">
+          <img src={about} alt="Header Image" />
+        </div>
+        <div className="about-content">
+          <h2>About Us</h2>
+          <p>
+            Welcome to Veteran Benevolent Support Africa, a platform created to
+            serve retired military veterans. Our mission is to provide
+            comprehensive support and resources to empower veterans in their
+            service journey.We believe in enhancing the well-being, personal
+            growth, and professional success of veterans. Through our platform,
+            veterans have access to a wide range of resources that can improve
+            that.
+          </p>
+          <p>
+            At Veteran Benevolent Support Africa, we understand the unique
+            challenges faced by retired military veterans. Our user-friendly
+            platform offers a range of features and services to address diverse
+            needs. From health awareness and education to education and
+            training, entrepreneurship courses, employment, support services and
+            much more. Join us as we provide comprehensive support to retired
+            military veterans. Together, we can make a difference in the lives
+            of veterans in Africa.
+          </p>
+        </div>
       </div>
-      <div className="about-content">
-        <h2>About Us</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at lacus auctor, venenatis ex et, iaculis odio. In hac habitasse platea dictumst. Praesent euismod consectetur diam ut pellentesque. Vivamus ut elit euismod, semper urna vel, sagittis nulla. Ut aliquam eros at tristique dignissim. Nullam varius vitae ipsum in scelerisque. Sed dignissim massa enim, eu finibus tortor aliquet eget.</p>
-        <p>Morbi euismod odio in mi blandit auctor. Donec eu nunc ut mi gravida bibendum at id dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris dapibus ex vel efficitur laoreet. Fusce in bibendum nisi. Nam quis dolor et leo feugiat pharetra ut vel neque. Integer eget turpis nisi.</p>
-      </div>
-    </div>
-    <Footer/>
+      <Footer />
     </>
   );
 };
